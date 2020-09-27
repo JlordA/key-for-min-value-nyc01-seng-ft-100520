f649dff0815a514ce9bf4c31546308e2f0a7d7fb
placid-tier-4550 <james.ardery@gmail.com>
2020-09-27 20:53:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = nil
+  min_name = nil
   name_hash.each do |name, number|
     #hash_index = 0
     #while hash_index < name_hash.count do
