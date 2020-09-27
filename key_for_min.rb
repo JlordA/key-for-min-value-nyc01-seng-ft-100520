e288208f6d6a7ce567530eae4526854e8bf54b1c
placid-tier-4550 <james.ardery@gmail.com>
2020-09-27 20:51:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, number|
     #hash_index = 0
     #while hash_index < name_hash.count do
-      if min_value == nil || name_hash[number] < min_value
+      if min_value == nil || number < min_value
        #binding.pry
        min_value = name_hash[number]
       end
