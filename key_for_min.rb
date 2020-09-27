885c849386213cd808070ac715971f346ca55661
placid-tier-4550 <james.ardery@gmail.com>
2020-09-27 20:00:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |name, number|
+    min_value = nil
+    while number < name_hash.count do
+      if min_value == nil || name_hash[number] < min_value
+        min_value = name_hash[number]
+    end
+  end
 end
